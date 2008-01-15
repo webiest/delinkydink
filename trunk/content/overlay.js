68006cb15768fd9484cc58b923a76777589ec66b
webiest <webiest@a663ca4a-bb43-0410-857d-db86d56fae08>
2008-01-15 06:46:59 +0000

    got a statusbar thing going. no need for drag and drop, can just click on current page.

--- trunk/content/overlay.js
+++ trunk/content/overlay.js
@@ -10,3 +10,11 @@ var Delinkydink = {
 };
 
 window.addEventListener("load", function(e) { Delinkydink.onLoad(e); }, false); 
+
+function getURL() {
+	
+  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].
+           getService(Components.interfaces.nsIWindowMediator);
+  var recentWindow = wm.getMostRecentWindow("navigator:browser");
+  return  recentWindow ? recentWindow.content.document.location : null;
+  }
