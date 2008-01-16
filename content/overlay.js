034d8a9131ae712d0f2c8f34c82404e05310ad80
webiest <webiest@a663ca4a-bb43-0410-857d-db86d56fae08>
2008-01-16 03:17:29 +0000

    added options, ajax call, code cleanup

--- content/overlay.js
+++ content/overlay.js
@@ -1,20 +1,58 @@
+var prefs = Components.classes["@mozilla.org/preferences-service;1"].
+	                getService(Components.interfaces.nsIPrefBranch);
 var Delinkydink = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-  },
-
-  onMenuItemCommand: function() {
-    window.open("chrome://delinkydink/content/delinkydink.xul", "", "chrome");
-  }
+	onLoad: function() {
+	this.initialized = true;
+	},
+  
+	onClickCommand: function() {
+		req = new XMLHttpRequest();
+		network_link='http://del.icio.us/network/'+prefs.getCharPref("extensions.delinkydink.username");
+		alert (network_link);
+		req.open('get', network_link, true);
+		req.setRequestHeader('User-Agent', 'blah blah');
+		req.setRequestHeader('Accept-Charset','utf-8');
+		req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+		req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+		Log.log('unr = false');
+		req.onreadystatechange=state_Change;
+		req.send(null); 		
+	},
 };
 
 window.addEventListener("load", function(e) { Delinkydink.onLoad(e); }, false); 
 
 function getURL() {
-	
   var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].
            getService(Components.interfaces.nsIWindowMediator);
   var recentWindow = wm.getMostRecentWindow("navigator:browser");
   return  recentWindow ? recentWindow.content.document.location : null;
+}
+
+
+function state_Change() {
+	if (req.readyState==4) {
+		alert ('readystate=4');
+		alert (req.responseText);
+		var t=req.responseText.getElementsByTagName("title");
+		alert(t);
+	if (req.status==200){
+    // ...some code here...
+    } else {
+		alert("Problem retrieving XML data");
+    }
   }
+}
+  
+  
+var Log =
+{
+  // mozilla log service
+  serv: Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService),
+  /**
+   * @param {String} message
+   */
+  log: function(message){
+    this.serv.logStringMessage('DLD: '+message);
+  }
+};  
