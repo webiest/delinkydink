9106ce2b5f55de042bd7faad5c5af397c1bd221b
webiest <webiest@a663ca4a-bb43-0410-857d-db86d56fae08>
2008-01-22 06:06:09 +0000

    update to bat file

--- create_xpi.bat
+++ create_xpi.bat
@@ -1,4 +1,4 @@
 del delinkydink.xpi
 del delinkydink.zip
-zip -r delinkydink * -x \create_xpi.bat \.svn* \delinkydink@mozilla.doslash.org \*.bak
+zip -r delinkydink * -x \create_xpi.bat \.svn* \delinkydink@mozilla.doslash.org \*.bak \Thumbs.db
 mv delinkydink.zip delinkydink.xpi
